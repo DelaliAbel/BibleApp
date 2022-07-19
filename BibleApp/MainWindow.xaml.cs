a856811d846efb3408c1cbf8253f4771f397d662
Elbelto <delaliabel@gmail.com>
2022-07-19 12:55:38 +0100

    Adding Bible Project

---
+++ BibleApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BibleApp.ViewModel;
+using BibleApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+    
+namespace BibleApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            WorkPanel.Content = new BibleView();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
